ca56f5e8259933636cbaa3880140228f4acb614a
Esteban Flores <ejfr1985@gmail.com>
2019-07-06 10:09:33 -0500

    Views not recycled

--- app/src/main/java/com/ejfr1985/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/ejfr1985/coderswag/Controller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.AbstractThreadedSyncAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.ejfr1985.coderswag.Adapters.CategoryAdapter
 import com.ejfr1985.coderswag.Model.Category
 import com.ejfr1985.coderswag.R
 import com.ejfr1985.coderswag.Services.DataService
@@ -11,19 +12,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories
-        )
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
-
 
     }
 }
