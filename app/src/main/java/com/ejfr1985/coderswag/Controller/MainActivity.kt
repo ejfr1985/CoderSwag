16bf4269bf9aa379f89925b9b5907f38407ed077
Esteban Flores <ejfr1985@gmail.com>
2019-07-13 17:42:28 -0500

    Recycler Views

--- app/src/main/java/com/ejfr1985/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/ejfr1985/coderswag/Controller/MainActivity.kt
@@ -3,8 +3,10 @@ package com.ejfr1985.coderswag.Controller
 import android.content.AbstractThreadedSyncAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import com.ejfr1985.coderswag.Adapters.CategoryAdapter
+import com.ejfr1985.coderswag.Adapters.CategoryRecyclerAdapter
 import com.ejfr1985.coderswag.Model.Category
 import com.ejfr1985.coderswag.R
 import com.ejfr1985.coderswag.Services.DataService
@@ -12,14 +14,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecyclerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecyclerAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager =  LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
     }
 }
